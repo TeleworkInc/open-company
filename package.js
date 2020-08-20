1e069119dc3d7dabcfc2865f092f3061987bd470
Christian Lewis <ctjl96@gmail.com>
2020-08-20 02:22:49 -0500

    Added travis config, bumped .gnv config, added package.js

--- package.js
+++ package.js
@@ -3,21 +3,37 @@
  */
 /**
  * @fileoverview
- * Get package.json object.
+ * Get package.json object. This is depended on by `boot.js`, and cannot contain
+ * any third-party modules.
  */
 
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+/**
+ * The absolute dirname of this package.
+ */
+export const PACKAGE_ROOT = path.dirname(fileURLToPath(import.meta.url));
 
 /**
  * Read the package.json object from the current directory.
  *
+ * @param {boolean} fromRoot
+ * Set to `true` to refer to gnv's package.json, not the package.json in the
+ * current working directory as indicated by `process.cwd()`.
+ *
  * @return {object} package
  * The package.json object.
  */
-export const readPackageJson = () => JSON.parse(
+export const readPackageJson = (fromRoot = false) => JSON.parse(
     fs.readFileSync(
-        path.resolve(process.cwd(), 'package.json'),
+        path.resolve((
+            fromRoot
+                ? PACKAGE_ROOT
+                : process.cwd()
+        ),
+        'package.json'),
     ),
 );
 
@@ -25,12 +41,28 @@ export const readPackageJson = () => JSON.parse(
  * @param {object} obj
  * The new package.json object to serialize and write.
  *
+ * @param {boolean} toRoot
+ * Set to `true` to write to gnv's package.json, rather than the package.json in
+ * the current working directory as indicated by `process.cwd()`.
+ *
  * @param {number} spaces
  * The number of spaces to use for tabs in JSON.stringify. Defaults to 2.
- *
  * @return {void}
  */
-export const writePackageJson = (obj, spaces = 2) => fs.writeFileSync(
-    path.resolve(process.cwd(), 'package.json'),
-    JSON.stringify(obj, null, 2),
+export const writePackageJson = (obj, toRoot = false, spaces = 2) => (
+  fs.writeFileSync(
+      path.resolve((
+        toRoot
+            ? PACKAGE_ROOT
+            : process.cwd()
+      ), 'package.json'),
+      JSON.stringify(obj, null, 2),
+  )
 );
+
+/**
+ * Get the version of this package as defined in package.json.
+ *
+ * @return {string} version
+ */
+export const getVersion = () => readPackageJson(true).version;
